932f1cb04ca5dd26250fbdc22c63bda856ccab23
Sven Schultschik <sven@schultschik.de>
2011-05-29 23:05:02 +0200

    Some starting help for benzea

--- plg_swsetgroup.php
+++ plg_swsetgroup.php
@@ -7,3 +7,24 @@
  * To change this template use File | Settings | File Templates.
  */
  
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class plgUserSwsetgroup extends JPlugin
+{
+    /**
+     * This event is triggered before an update of a user record.
+     * Password in $user array is already hashed at this point.
+     * @param  $user An associative array of the columns in the user table (current values).
+     * @param  $isnew Boolean to identify if this is a new user (true - insert) or an existing one (false - update)
+     * @return true
+     */
+    public function onBeforeStoreUser( $user, $isnew ){
+
+        //$user['email'] get the user email adress
+        //$this->param->get(preg, array); get the regular expressions from backend
+        return true;
+    }
+}
