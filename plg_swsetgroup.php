81bd2d612167fbd8070c5eec025c34cbd31087c8
Benjamin Berg <benjamin@sipsolutions.net>
2011-06-02 20:35:54 +0200

    Rewrite just for the heck of it.

--- plg_swsetgroup.php
+++ plg_swsetgroup.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: sven
- * Date: 28.05.11
- * Time: 14:14
- * To change this template use File | Settings | File Templates.
+ *
  */
  
 // No direct access
@@ -21,22 +17,28 @@ class plgUserSwsetgroup extends JPlugin
      * @param  $isnew Boolean to identify if this is a new user (true - insert) or an existing one (false - update)
      * @return  bool    true on success
      */
-    public function onBeforeStoreUser( $user, $isnew ){
+    public function onBeforeStoreUser( $user, $isnew ) {
         //get the config
         $conf = $this->param->get( 'preg', array() );
         //perform the preg_match
-        foreach ( $conf as $v){
-            //put him into the group when e-mail is correct
-            if( preg_match('#'.$v['preg'].'#', $user['email'] ) && $isnew){
-                $user['groups'][] = $v['group'];
-            }elseif ( preg_match('#'.$v['preg'].'#', $user['email'] ) && !$isnew){
-                //find a way to put him after a mail verification into the group
+        foreach ($conf as $v) {
+            if ($isnew) {
+                // The user is new
 
-            }//remove the group if the mail-address get changed back
-            elseif ( !preg_match('#'.$v['preg'].'#', $user['email'] ) && !$isnew){
-                foreach ( $user['groups'] as $k => $vg){
-                    if ( $vg == $v['group'] ){
-                        unset ( $user['groups'][$k] );
+                // Add to the group if the regular expression matches
+                if (preg_match('#'.$v['preg'].'#', $user['email'])) {
+                    $user['groups'][] = $v['group'];
+                }
+            } else {
+                // The user already exists
+
+                if (preg_match('#'.$v['preg'].'#', $user['email'])) {
+                    // Send verification e-mail from here
+                } else {
+                    // Remove user from group.
+                    $k = array_search($v['group'], $user['groups']);
+                    if ($k !== FALSE) {
+                        unset ($user['groups'][$k]);
                     }
                 }
             }
