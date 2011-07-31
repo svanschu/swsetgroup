c6dc70ec3530384177018bb6308b179587539206
Sven Schultschik <sven@schultschik.de>
2011-07-31 02:00:37 +0200

    jos -> #_

--- com_swsetgroup/administrator/components/com_swsetgroup/install.sql
+++ com_swsetgroup/administrator/components/com_swsetgroup/install.sql
@@ -4,7 +4,7 @@
 -- @copyright   Copyright (C) 2011 Benjamin Berg & Sven Schultschik. All rights reserved.
 -- @license             GNU General Public License version 2 or later
 
-CREATE TABLE IF NOT EXISTS `jos_swsetgroup_pending` (
+CREATE TABLE IF NOT EXISTS `#__swsetgroup_pending` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `uid` int(11) NOT NULL COMMENT '#__users',
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
