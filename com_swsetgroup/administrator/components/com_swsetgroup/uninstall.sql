766423b22bf708d5a161085034627edc066379fa
Sven Schultschik <sven@schultschik.de>
2011-08-01 20:28:18 +0200

    jos  -> #_

--- com_swsetgroup/administrator/components/com_swsetgroup/uninstall.sql
+++ com_swsetgroup/administrator/components/com_swsetgroup/uninstall.sql
@@ -4,4 +4,4 @@
 -- @copyright   Copyright (C) 2011 Benjamin Berg & Sven Schultschik. All rights reserved.
 -- @license             GNU General Public License version 2 or later
 
-DROP TABLE `jos_swsetgroup_pending`;
+DROP TABLE `#__swsetgroup_pending`;
